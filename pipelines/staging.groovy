374f769c37c9bd4d4217df85d2c5ec321fbdd345
rsafronov <electroma@gmail.com>
2016-12-01 17:21:10 -0500

    pipeline fix

--- pipelines/staging.groovy
+++ pipelines/staging.groovy
@@ -15,22 +15,12 @@ node {
           sh "git clean -ffdx"
         }
 
-        if (env.USE_GLOBAL_LIB) {
-          ecrInst = new ecr()
-          gitInst = new git()
-          npmInst = new npm()
-          slackInst = new slack()
-          convoxInst = new convox()
-          templateInst = new template()
-        } else {
-          sh "curl -L https://dl.bintray.com/buildit/maven/jenkins-pipeline-libraries-${env.PIPELINE_LIBS_VERSION}.zip -o lib.zip && echo 'A' | unzip -o lib.zip"
-          ecrInst = load "lib/ecr.groovy"
-          gitInst = load "lib/git.groovy"
-          npmInst = load "lib/npm.groovy"
-          slackInst = load "lib/slack.groovy"
-          convoxInst = load "lib/convox.groovy"
-          templateInst = load "lib/template.groovy"
-        }
+        ecrInst = new ecr()
+        gitInst = new git()
+        npmInst = new npm()
+        slackInst = new slack()
+        convoxInst = new convox()
+        templateInst = new template()
 
         domainName = "${env.MONGO_HOSTNAME}".substring(8)
         registryBase = "006393696278.dkr.ecr.${env.AWS_REGION}.amazonaws.com"
@@ -52,9 +42,9 @@ node {
         checkout scm
 
         // global for exception handling
-        shortCommitHash = git.getShortCommit()
-        commitMessage = git.getCommitMessage()
-        version = npm.getVersion()
+        shortCommitHash = gitInst.getShortCommit()
+        commitMessage = gitInst.getCommitMessage()
+        version = npmInst.getVersion()
       }
 
       stage ('Install') {
@@ -77,7 +67,7 @@ node {
       stage ('Docker Image Build') {
         tag = "${version}-${shortCommitHash}-${env.BUILD_NUMBER}"
         image = docker.build("${appName}:${tag}", '.')
-        ecr.authenticate(env.AWS_REGION)
+        ecrInst.authenticate(env.AWS_REGION)
       }
 
       stage ('Docker Push') {
@@ -88,7 +78,7 @@ node {
 
       stage ('Deploy To AWS') {
         tmpFile = UUID.randomUUID().toString() + ".tmp"
-        ymlData = template.transform(readFile("docker-compose.yml.template"), [tag: tag, registry_base: registryBase, mongo_url: mongoUrl, db_context: dbContext, server_url: serverUrl, server_port: serverPort])
+        ymlData = templateInst.transform(readFile("docker-compose.yml.template"), [tag: tag, registry_base: registryBase, mongo_url: mongoUrl, db_context: dbContext, server_url: serverUrl, server_port: serverPort])
         writeFile(file: tmpFile, text: ymlData)
 
         sh "convox login ${env.CONVOX_RACKNAME} --password ${env.CONVOX_PASSWORD}"
@@ -98,8 +88,8 @@ node {
 
       stage ('Run Functional Acceptance Tests') {
         // wait until the app is deployed
-        convox.waitUntilDeployed("${appName}-staging")
-        convox.ensureSecurityGroupSet("${appName}-staging", env.CONVOX_SECURITYGROUP)
+        convoxInst.waitUntilDeployed("${appName}-staging")
+        convoxInst.ensureSecurityGroupSet("${appName}-staging", env.CONVOX_SECURITYGROUP)
         sh "NODE_ENV='acceptance' npm run accept"
       }
 
@@ -107,12 +97,12 @@ node {
         docker.withRegistry(registry) {
           image.push("latest")
         }
-        slack.notify("Deployed to Staging", "Commit <${gitUrl}/commits/${shortCommitHash}|${shortCommitHash}> has been deployed to <${appUrl}|${appUrl}>\n\n${commitMessage}", "good", "http://i3.kym-cdn.com/entries/icons/original/000/006/536/illuminati-conspiracy.jpg", slackChannel)
+        slackInst.notify("Deployed to Staging", "Commit <${gitUrl}/commits/${shortCommitHash}|${shortCommitHash}> has been deployed to <${appUrl}|${appUrl}>\n\n${commitMessage}", "good", "http://i3.kym-cdn.com/entries/icons/original/000/006/536/illuminati-conspiracy.jpg", slackChannel)
       }
     }
     catch (err) {
       currentBuild.result = "FAILURE"
-      slack.notify("Error while deploying to Staging", "Commit <${gitUrl}/commits/${shortCommitHash}|${shortCommitHash}> failed to deploy to <${appUrl}|${appUrl}>", "danger", "http://i3.kym-cdn.com/entries/icons/original/000/004/240/CandleCove.jpg", slackChannel)
+      slackInst.notify("Error while deploying to Staging", "Commit <${gitUrl}/commits/${shortCommitHash}|${shortCommitHash}> failed to deploy to <${appUrl}|${appUrl}>", "danger", "http://i3.kym-cdn.com/entries/icons/original/000/004/240/CandleCove.jpg", slackChannel)
       throw err
     }
   }
