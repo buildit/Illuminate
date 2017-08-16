fc439c69aff30fc39119bf0cf804eb977f9bc550
Ben Hernandez <ben.hernandez@wipro.com>
2017-08-16 12:01:29 -0600

    adding defect indicator for Sev 1 defects

--- services/statusIndicators/defect/index.js
+++ services/statusIndicators/defect/index.js
@@ -1,8 +1,16 @@
+const checkForAnySev1DefectsToday = require('./checkForAnySev1DefectsToday');
+const constants = require('../../../util/constants');
+const dataStore = require('../../datastore/mongodb');
+const R = require('ramda');
+const CO = require('CO');
+
 module.exports = {
-  getStatuses(/* project, projectPath */) {
-    const statusIndicators = [];
-    return Promise.all(statusIndicators).then(statuses => {
-      return statuses.filter(status => status);
+  getStatuses(project, projectPath) {
+    return CO(function* () {
+      const statusIndicators = [];
+      const defects = yield dataStore.getAllData(projectPath, constants.SUMMARYDEFECT)
+      statusIndicators.push(checkForAnySev1DefectsToday.evaluate(project, R.clone(defects)));
+      return statusIndicators.filter(statusIndicators => statusIndicators);
     });
   },
 };
